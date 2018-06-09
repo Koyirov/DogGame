8b8aaa661cfa05685141e23ffbda42425fb0af99
MisterShoh <herr_shoh@mail.ru>
2018-06-09 09:15:10 +0200

    Für schnelle Arbeit GUI Aufruf ohne Abfrage getan

--- src/main/scala/de/htwg/se/DogGame/util/Maingame.scala
+++ src/main/scala/de/htwg/se/DogGame/util/Maingame.scala
@@ -7,10 +7,10 @@ import main.scala.de.htwg.se.DogGame.view.SwingGui
 object Maingame {
 
   def main(args: Array[String]) {
-
     var guiBoolean = false
     println("gui?")
     var input = scala.io.StdIn.readLine()
+    //var input = "gui"
     if (input == "gui") {
       guiBoolean = true
     }
