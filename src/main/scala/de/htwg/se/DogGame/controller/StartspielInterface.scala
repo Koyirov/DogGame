b1ec4939ce65ef4b38bd60a929f361181156c5b9
MisterShoh <herr_shoh@mail.ru>
2018-06-13 21:07:01 +0200

    Interfaces fuer View und Controller Komponente definiert

---
+++ src/main/scala/de/htwg/se/DogGame/controller/StartspielInterface.scala
@@ -0,0 +1,10 @@
+package main.scala.de.htwg.se.DogGame.controller
+
+import main.scala.de.htwg.se.DogGame.view.SwingGui
+
+trait StartspielInterface {
+  
+  var guiIns = new SwingGui()
+  
+  def start_spiel()
+}
---
+++ src/main/scala/de/htwg/se/DogGame/view/ViewInterfaces.scala
@@ -0,0 +1,13 @@
+package main.scala.de.htwg.se.DogGame.view
+
+import scala.collection.mutable.ArrayBuffer
+import main.scala.de.htwg.se.DogGame.model.Lauffeld
+import main.scala.de.htwg.se.DogGame.model.Spieler
+
+trait SwingGuiInterfaces {
+  def update_Spiel_Brett(lF: Lauffeld, p: ArrayBuffer[Spieler], turn: Int)
+}
+
+trait TuiInterfaces {
+  def tui_v1(lF: Lauffeld, p: ArrayBuffer[Spieler])
+}
