e2163afe49d1e93b3046827509b0ac9b363dc941
MisterShoh <herr_shoh@mail.ru>
2018-06-18 22:46:37 +0200

    alle Dependencies definiert

--- src/main/scala/de/htwg/se/DogGame/DependencyModule.scala
+++ src/main/scala/de/htwg/se/DogGame/DependencyModule.scala
@@ -9,8 +9,20 @@ import main.scala.de.htwg.se.DogGame.controller.StartspielInterface
 import main.scala.de.htwg.se.DogGame.model.{FeldInterfaces, KartenInterfaces, 
   SpielbrettInterfaces, SpielerInterfaces, SpielfigurInterfaces}
 
+import main.scala.de.htwg.se.DogGame.view.{Fenster, SwingGui, Tui}
+import main.scala.de.htwg.se.DogGame.controller.Startspiel
+import main.scala.de.htwg.se.DogGame.model.{Lauffeld, Karten, 
+  Spielbrett, Spieler, Spielfigur}
+
 class DependencyModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
-
+    bind[FensterInterface].to[Fenster]
+    bind[SwingGuiInterface].to[SwingGui]
+    bind[TuiInterface].to[Tui]
+    bind[StartspielInterface].to[Startspiel]
+    bind[FeldInterfaces].to[Lauffeld]
+    bind[SpielbrettInterfaces].to[Spielbrett]
+    bind[SpielerInterfaces].to[Spieler]
+    bind[SpielfigurInterfaces].to[Spielfigur]
   }
 }
